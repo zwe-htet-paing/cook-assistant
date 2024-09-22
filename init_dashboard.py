b480129d57743a67d5c8b5d21c74d55ff162f04b
zwe-htet-paing <zwehtetpaing.works@gmail.com>
2024-09-22 19:32:08 +0700

    Update init flow

--- init_dashboard.py
+++ init_dashboard.py
@@ -1,18 +1,14 @@
-from prefect import flow, task
 import os
 import subprocess
 
-@task
 def initialize_grafana():
     # print("Initializing Grafana dashbord...")
     subprocess.run(["pipenv", "run", "python", "grafana/init.py"], check=True)
     return "Successful"
 
 # Define your flow
-@flow
 def main_flow():
     initialize_grafana_result = initialize_grafana()
-
     print("Grafana initialized:", initialize_grafana_result)
 
 # Run the flow
--- init_database.py
+++ init_database.py
@@ -1,15 +1,12 @@
-from prefect import flow, task
 import os
 import subprocess
 
-@task
 def prepare_database():
     # print("Preparing the database...")
     subprocess.run(["pipenv", "run", "python", "src/db_prep.py"], check=True)
     return "Successful"
 
 # Define your flow
-@flow
 def main_flow():
     prepare_database_result = prepare_database()
     print("Database prepared:", prepare_database_result)
